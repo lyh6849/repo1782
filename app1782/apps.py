82c4229be3934cd08c156a1b46b477ec2b998a5f
lyh6849 <lyh6849@gmail.com>
2022-11-13 23:05:17 -0500

    delete

--- app1782/apps.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1bd007af2b0dcb2ea5426cafa16da5a7e75d67b0c94a3ed0deef8c2383e6d344
-size 146
--- manage.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e3ac603c30a27e3ab7c41775f1aa0e614089f8b29d3ca7e7bfd6a829ce1bba2a
-size 667
