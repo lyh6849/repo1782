74e12dc64baadce2b1d9ae6c4f0a9472577140c4
lyh6849 <lyh6849@gmail.com>
2022-11-13 22:47:38 -0500

    r

---
+++ app1782/models.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31a744e15f7b5174ab10673626bf0da77617645344d5b33afb13c610b95df4e2
+size 2548
---
+++ project1782/wsgi.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb4630336d1566b0b21a41f36bb509e8aae71463e85dd965111041b33b8668c2
+size 399
