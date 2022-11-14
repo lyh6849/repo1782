e6ea6c602c6c091e9dc3a40f0e53f2301dca6b3d
lyh6849 <lyh6849@gmail.com>
2022-11-13 22:37:35 -0500

    T

---
+++ Procfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11f30e101645a5a4633345969b4f1997004872bac82cc560f0a0a16d89b1e2b4
+size 50
---
+++ project1782/settings.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e19c66c7fc4b9cd46e8bcce6be78e211fe4669a01e39ca50f873672ffb716752
+size 3412
