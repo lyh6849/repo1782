67d041252cfd72166922c3a349143e109b9aad4f
lyh6849 <lyh6849@gmail.com>
2022-11-13 22:37:04 -0500

    T

--- app1782/urls.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7bc3e3bb7909422e77b0162bbed10065047e047310f323c99a2f7b43bc08fd29
-size 4276
--- app1782/views.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:829344e3e7ea7e71865a117efb956d1cafb466017a20861e9abfb3b058626baf
-size 61555
