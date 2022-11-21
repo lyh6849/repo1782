798b69a5501881db6d2fe092afb071358b5dbd9f
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2022-11-21 13:21:51 -0500

    d

--- app1782/views.py
+++ app1782/views.py
@@ -431,15 +431,16 @@ def text_to_predict(text):
   sequence_mass=tf.keras.preprocessing.sequence.pad_sequences(sequence_mass, maxlen=max_len)
   #model.predict(np.expand_dims(sequence_mass[0], 0))[0]
   print(str(model.predict(np.expand_dims(sequence_mass[0], 0), train=False)))
-'''
+  return_array2=[]
+  return return_array2
+  '''
   return_array=[]
   for i in model.predict(np.expand_dims(sequence_mass[0], 0))[0]:
     return_array.append(round(i,2))
-  return_array2=[]
   for j in return_array:
     return_array2.append(round(j*100,0))
-'''  
-  return return_array2
+  '''  
+  
 
 def classifier(request):
   output=request.POST['ajax_input']
